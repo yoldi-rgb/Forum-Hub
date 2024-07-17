7325eb4575a1b57b7a77d270a0b282cf00538b19
yoldicarrada <carradayoldi@gmail.com>
2024-07-16 21:20:28 -0600

    change the sixtasis sql

--- src/main/resources/db/migration/V2__create-table-topicos.sql
+++ src/main/resources/db/migration/V2__create-table-topicos.sql
@@ -1,9 +1,9 @@
 CREATE TABLE topicos (
     id BIGINT PRIMARY KEY AUTO_INCREMENT,
     titulo VARCHAR(255) NOT NULL,
-    mensaje VARCHAR(255) NOT NULL,
+    mensaje TEXT NOT NULL,
     fecha_de_creacion DATETIME NOT NULL,
-    status status_enum NOT NULL,
+    status ENUM('ACTIVO', 'CERRADO') NOT NULL,
     usuario_id BIGINT,
     curso VARCHAR(255),
     FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
